c29c860f17b07c7a0500e8a16236b37899ba5bda
Dmitry Yakimenko <detunized@gmail.com>
2014-07-28 02:53:32 +0200

    Merging iterator

---
+++ src/main/scala/MergingIterator.scala
@@ -0,0 +1,28 @@
+package net.detunized.iteratorz
+
+import scala.math.Ordering
+
+class MergingIterator[A](i1: Iterator[A], i2: Iterator[A])(implicit ord: Ordering[A]) extends Iterator[A] {
+  private[this] val i1b = i1.buffered
+  private[this] val i2b = i2.buffered
+
+  override def hasNext: Boolean = i1b.hasNext || i2b.hasNext
+
+  override def next(): A = {
+    if (i1b.hasNext) {
+      if (i2b.hasNext) {
+        if (ord.lteq(i1b.head, i2b.head)) {
+          i1b.next()
+        } else {
+          i2b.next()
+        }
+      } else {
+        i1b.next()
+      }
+    } else if (i2b.hasNext) {
+      i2b.next()
+    } else {
+      Iterator.empty.next()
+    }
+  }
+}
---
+++ src/test/scala/MergingIteratorSpec.scala
@@ -0,0 +1,39 @@
+package net.detunized.iteratorz
+
+import org.specs2.mutable.Specification
+
+class MergingIteratorSpec extends Specification {
+  "MergingIterator" should {
+    "have no next on empty iterators" in {
+      check(Seq[Int](), Seq[Int]())
+    }
+
+    "return first sequence when second is empty" in {
+      check(Seq(1, 2, 3), Seq[Int]())
+    }
+
+    "return second sequence when first is empty" in {
+      check(Seq[Int](), Seq(1, 2, 3))
+    }
+
+    "return first sequence then second" in {
+      check(Seq(1, 1, 1), Seq(2, 2, 2))
+    }
+
+    "return second sequence then first" in {
+      check(Seq(2, 2, 2), Seq(1, 1, 1))
+    }
+  }
+
+  private def check[A](a: Seq[A], b: Seq[A])(implicit ord: Ordering[A]) = {
+    val i = new MergingIterator(a.iterator, b.iterator)
+
+    (a ++ b).sorted foreach { x =>
+      i.hasNext should beTrue
+      i.next() shouldEqual x
+    }
+
+    i.hasNext should beFalse
+    i.next() should throwA[NoSuchElementException]
+  }
+}
