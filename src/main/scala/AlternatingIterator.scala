0cc0cffb28b7bb8d887333c64ade5631b4fd0731
Dmitry Yakimenko <detunized@gmail.com>
2014-08-07 03:09:49 +0200

    Alternating iterator via Zipping

--- src/main/scala/AlternatingIterator.scala
+++ src/main/scala/AlternatingIterator.scala
@@ -1,22 +1,4 @@
 package net.detunized.iteratorz
 
-class AlternatingIterator[A](i1: Iterator[A], i2: Iterator[A]) extends CombiningIterator[A] {
-  private[this] var first = true
-
-  override def hasNext: Boolean = i1.hasNext || i2.hasNext
-  override def next(): A = {
-    if (first && i1.hasNext) {
-      first = false
-      i1.next()
-    } else if (!first && i2.hasNext) {
-      first = true
-      i2.next()
-    } else if (i1.hasNext) {
-      i1.next()
-    } else if (i2.hasNext) {
-      i2.next()
-    } else {
-      Iterator.empty.next()
-    }
-  }
-}
+class AlternatingIterator[A](i1: Iterator[A], i2: Iterator[A])
+  extends ZippingIterator[A](i1, i2)((_, _) => true)
