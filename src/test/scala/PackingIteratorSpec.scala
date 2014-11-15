35a944e4dd703ed6b03b2ff7e94364db4fbc2a1a
Dmitry Yakimenko <detunized@gmail.com>
2014-11-15 03:42:53 +0100

    PackingIteratorSpec uses IteratorSpec

--- src/test/scala/PackingIteratorSpec.scala
+++ src/test/scala/PackingIteratorSpec.scala
@@ -1,44 +1,27 @@
 package net.detunized.iteratorz
 
-import org.specs2.mutable.Specification
-
-class PackingIteratorSpec extends Specification {
-  "have no next on an empty iterator" in {
-    val i = mk(Seq[Int]())
-
-    i.hasNext should beFalse
-    i.next() should throwA[NoSuchElementException]
-  }
+class PackingIteratorSpec extends IteratorSpec[(String, Int), Iterator[(String, Int)]] {
+  type S = String
 
   "pack single elements" in {
-    check(Seq("A"), Seq(("A", 1)))
-    check(Seq("A", "B"), Seq(("A", 1), ("B", 1)))
-    check(Seq("A", "B", "C"), Seq(("A", 1), ("B", 1), ("C", 1)))
+    check(mk("A"), ("A", 1))
+    check(mk("A", "B"), ("A", 1), ("B", 1))
+    check(mk("A", "B", "C"), ("A", 1), ("B", 1), ("C", 1))
   }
 
   "pack repeating elements" in {
-    check(Seq("A", "A"), Seq(("A", 2)))
-    check(Seq("A", "A", "A"), Seq(("A", 3)))
+    check(mk("A", "A"), ("A", 2))
+    check(mk("A", "A", "A"), ("A", 3))
   }
 
   "pack mixed single and repeated elements" in {
-    check(Seq("A", "A", "B"), Seq(("A", 2), ("B", 1)))
-    check(Seq("A", "B", "B"), Seq(("A", 1), ("B", 2)))
-    check(Seq("A", "B", "B", "C", "C", "C"), Seq(("A", 1), ("B", 2), ("C", 3)))
-    check(Seq("A", "A", "A", "B", "B", "C"), Seq(("A", 3), ("B", 2), ("C", 1)))
-    check(Seq("A", "A", "B", "C", "C", "C"), Seq(("A", 2), ("B", 1), ("C", 3)))
+    check(mk("A", "A", "B"), ("A", 2), ("B", 1))
+    check(mk("A", "B", "B"), ("A", 1), ("B", 2))
+    check(mk("A", "B", "B", "C", "C", "C"), ("A", 1), ("B", 2), ("C", 3))
+    check(mk("A", "A", "A", "B", "B", "C"), ("A", 3), ("B", 2), ("C", 1))
+    check(mk("A", "A", "B", "C", "C", "C"), ("A", 2), ("B", 1), ("C", 3))
   }
 
-  private def mk[A](s: Seq[A]) = new PackingIterator(s.iterator)
-  private def check[A](s: Seq[A], expected: Seq[A]) = {
-    val i = mk(s)
-
-    expected foreach { x =>
-      i.hasNext should beTrue
-      i.next() shouldEqual x
-    }
-
-    i.hasNext should beFalse
-    i.next() should throwA[NoSuchElementException]
-  }
+  protected def mkEmpty = mk()
+  private[this] def mk(s: S*) = new PackingIterator(s.iterator)
 }
