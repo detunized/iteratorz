d74b78973c133f03f25a6f129d510ff0bc334e08
Dmitry Yakimenko <detunized@gmail.com>
2014-11-16 19:34:01 +0100

    RepeatingIteratorSpec uses IteratorSpec

--- src/test/scala/RepeatingIteratorSpec.scala
+++ src/test/scala/RepeatingIteratorSpec.scala
@@ -1,64 +1,52 @@
 package net.detunized.iteratorz
 
-import org.specs2.mutable.Specification
-
-class RepeatingIteratorSpec extends Specification {
+class RepeatingIteratorSpec extends IteratorSpec[Int] {
   "is empty on an empty iterator" in {
-    checkIsEmpty(mk(Seq[Int]())(x => 1))
+    mk()(_ => 1) must beEmpty
   }
 
   "is empty with zero repeat count" in {
-    checkIsEmpty(mk(Seq(1, 2, 3))(x => 0))
+    mk(1, 2, 3)(_ => 0) must beEmpty
   }
 
   "hasNext throws on negative count" in {
-    mk(Seq(1, 2, 3))(x => -1).hasNext should throwA[IllegalArgumentException]
+    mk(1, 2, 3)(_ => -1).hasNext must throwA[IllegalArgumentException]
   }
 
   "next throws on negative count" in {
-    mk(Seq(1, 2, 3))(x => -1).next() should throwA[IllegalArgumentException]
+    mk(1, 2, 3)(_ => -1).next() must throwA[IllegalArgumentException]
   }
 
   "returns the same sequence with repeat count of 1" in {
-    val s = Seq(1, 2, 3)
-    mk(s)(x => 1).toList shouldEqual s
+    mk(1, 2, 3)(_ => 1) must expandTo(1, 2, 3)
   }
 
   "repeats elements twice" in {
-    val s = Seq(1, 2, 3)
-    mk(s)(x => 2).toList shouldEqual Seq(1, 1, 2, 2, 3, 3)
+    mk(1, 2, 3)(_ => 2) must expandTo(1, 1, 2, 2, 3, 3)
   }
 
   "repeats elements variying number of times" in {
-    val s = Seq(1, 2, 3, 4)
-    mk(s)(x => x).toList shouldEqual Seq(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
+    mk(1, 2, 3, 4)(x => x) must expandTo(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
   }
 
   "skips elements in the beginning" in {
-    val s = Seq(1, 2, 3, 4, 5)
-    mk(s)(x => if (x == 1 || x == 2) 0 else 1).toList shouldEqual Seq(3, 4, 5)
+    mk(1, 2, 3, 4, 5)(x => if (x == 1 || x == 2) 0 else 1) must expandTo(3, 4, 5)
   }
 
   "skips elements in the middle" in {
-    val s = Seq(1, 2, 3, 4, 5)
-    mk(s)(x => if (x == 2 || x == 4) 0 else 1).toList shouldEqual Seq(1, 3, 5)
+    mk(1, 2, 3, 4, 5)(x => if (x == 2 || x == 4) 0 else 1) must expandTo(1, 3, 5)
   }
 
   "skips elements in the end" in {
-    val s = Seq(1, 2, 3, 4, 5)
-    mk(s)(x => if (x == 4 || x == 5) 0 else 1).toList shouldEqual Seq(1, 2, 3)
+    mk(1, 2, 3, 4, 5)(x => if (x == 4 || x == 5) 0 else 1) must expandTo(1, 2, 3)
   }
 
   "skips and repeats elements" in {
-    val s = Seq(1, 2, 3, 4, 5)
-    val m = Map(1 -> 0, 2 -> 3, 3 -> 1, 4 -> 0, 5 -> 2)
-    mk(s)(m(_)).toList shouldEqual Seq(2, 2, 2, 3, 5, 5)
+    mk(1, 2, 3, 4, 5)(Map(1 -> 0, 2 -> 3, 3 -> 1, 4 -> 0, 5 -> 2)) must expandTo(2, 2, 2, 3, 5, 5)
   }
 
-  private def mk[A](s: Seq[A])(f: A => Int) = new RepeatingIterator(s.iterator)(f)
+  protected[this] def mkEmpty = mk()(_ => 1)
 
-  private def checkIsEmpty[A](i: RepeatingIterator[A]) = {
-    i.hasNext should beFalse
-    i.next() should throwA[NoSuchElementException]
-  }
+  private[this] def mk(s: T*)(f: T => Int) =
+    new RepeatingIterator(s.iterator)(f)
 }
