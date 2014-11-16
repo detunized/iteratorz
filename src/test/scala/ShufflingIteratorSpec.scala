40bbc754df1a05922f20bceaa78c7f5c7570cec7
Dmitry Yakimenko <detunized@gmail.com>
2014-11-16 19:01:10 +0100

    ShufflingIteratorSpec uses IteratorSpec

--- src/test/scala/ShufflingIteratorSpec.scala
+++ src/test/scala/ShufflingIteratorSpec.scala
@@ -1,34 +1,23 @@
 package net.detunized.iteratorz
 
-import org.specs2.mutable.Specification
-
-class ShufflingIteratorSpec extends Specification {
-  "have no next on an empty iterator" in {
-    checkEmpty(1)
-  }
-
+class ShufflingIteratorSpec extends IteratorSpec[Int] {
   "throw on zero window size" in {
-    mkEmpty(0) should throwA[IllegalArgumentException]
+    mk()(0) must throwA[IllegalArgumentException]
   }
 
   "throw on negative window size" in {
-    mkEmpty(-1) should throwA[IllegalArgumentException]
+    mk()(-1) must throwA[IllegalArgumentException]
   }
 
   "should return the same sequence with window of 1" in {
-    mk(Seq(1, 2, 3), 1).toList shouldEqual List(1, 2, 3)
+    mk(1, 2, 3)(1) must expandTo(1, 2, 3)
   }
 
   // TODO: Figure out how to test shuffling in a deterministic way!
   //       Maybe use a fake random generator.
 
-  private def mk[A](s: Seq[A], windowSize: Int) = new ShufflingIterator(s.iterator, windowSize)
-  private def mkEmpty[A](windowSize: Int) = mk(Seq[Int](), windowSize)
+  protected[this] def mkEmpty = mk()(1)
 
-  private def checkEmpty(windowSize: Int) = {
-    val i = mkEmpty(windowSize)
-
-    i.hasNext should beFalse
-    i.next() should throwA[NoSuchElementException]
-  }
+  private[this] def mk(s: T*)(windowSize: Int) =
+    new ShufflingIterator(s.iterator, windowSize)
 }
