80cbee9756c0ad21aeb4e0801a90113b3b831692
Dmitry Yakimenko <detunized@gmail.com>
2014-11-20 03:04:49 +0100

    Minor cleanup

--- src/main/scala/ShufflingIterator.scala
+++ src/main/scala/ShufflingIterator.scala
@@ -3,4 +3,4 @@ package net.detunized.iteratorz
 import scala.util.Random
 
 class ShufflingIterator[A](source: Iterator[A], windowSize: Int, rng: Random = new Random())
-  extends PoolingIterator[A](source, windowSize)(rng.shuffle(_))
+  extends PoolingIterator[A](source, windowSize)(rng.shuffle)
